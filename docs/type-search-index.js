4bf5ebd424be7762572081242722731e13c3e038
Yaelsinsky <112461512+Yaelsinsky@users.noreply.github.com>
2023-11-23 20:27:42 -0600

    Adición de javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"poop12","l":"Alumno"},{"p":"poop12","l":"Cuenta"},{"p":"poop12","l":"HilosCeros"},{"p":"poop12","l":"HilosR"},{"p":"poop12","l":"HilosSaltoDeLinea"},{"p":"poop12","l":"HilosT"},{"p":"poop12","l":"HilosUnos"},{"p":"poop12","l":"POOP12"}];updateSearchResults();
